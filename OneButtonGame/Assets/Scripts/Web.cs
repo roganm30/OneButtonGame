6bb816ced6cd3bb11209d383009eb09ddec5825e
Rogan <148461496+roganm30@users.noreply.github.com>
2026-05-05 13:10:01 -0400

    skateboarding sounds

--- OneButtonGame/Assets/Scripts/Web.cs
+++ OneButtonGame/Assets/Scripts/Web.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using FMODUnity;
 
 public class Web : MonoBehaviour
 {
@@ -6,25 +7,37 @@ public class Web : MonoBehaviour
     SpriteRenderer sr;
     public bool spriteEnabled = true;
 
+    StudioEventEmitter emitter;
+    public float parameter = 1;
+
     void Start()
     {
         spider = GetComponentInParent<Spider>();
         sr = GetComponent<SpriteRenderer>();
+
+        emitter = GetComponent<StudioEventEmitter>();
     }
+
 
     private void Update()
     {
         sr.enabled = spriteEnabled;
 
-        if (spider.attached)
+        if (spider.attached == true)
         {
-            GetComponent<FMODUnity.StudioEventEmitter>().Play();
+            emitter.SetParameter("Skateboard", 1);
+        } 
+        else if (spider.attached == false)
+        {
+            emitter.SetParameter("Skateboard", 0);
         }
+
+        
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log("snap web");
+        UnityEngine.Debug.Log("snap web");
         spider.attached = false;
         spriteEnabled = false;
     }
